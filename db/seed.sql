31ebcd91b2ed6fc5e8f42167a2ade366cbbc1643
ArshSharifat <arshiasharifat@gmail.com>
2018-11-15 15:17:16 -0800

    beta

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('double burger');
+INSERT INTO burgers (burger_name) VALUES ('guacamole burger');
